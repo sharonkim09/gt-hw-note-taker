0c48d183c53f0d17437bd37543ed07b64bc782de
Sharon Kim <sharonkim1709@gmail.com>
2020-07-05 21:13:58 -0400

    updated readme

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ const app = express()
 // port
 const PORT = process.env.PORT || 3000
 
+// data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// functionalities
+// GET ROUTE
+// POST ROUTE
+// DELETE ROUTE
+
+
 // listener
 app.listen(PORT, ()=>{
     console.log(`Listening on http://localhost:${PORT}`)
