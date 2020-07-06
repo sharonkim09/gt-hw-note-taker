952950b78bec81312d14175f748de0b2747c49c1
Sharon Kim <sharonkim1709@gmail.com>
2020-07-05 21:41:16 -0400

    successfully added get route to display html

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 // express
 const express = require("express")
-
+// path package
+const path = require("path")
+// file package to sendFile
+const fs = require("fs")
 // instance of express
 const app = express()
-
+// const
 // port
 const PORT = process.env.PORT || 3000
 
@@ -13,6 +16,13 @@ app.use(express.json());
 
 // functionalities
 // GET ROUTE
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./public/index.html"))
+})
+
+app.get("/notes",(req,res)=>{
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+})
 // POST ROUTE
 // DELETE ROUTE
 
